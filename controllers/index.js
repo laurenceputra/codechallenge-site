67cb4290db16d4e7363c9cae5a055f15bb8574cc
Laurence Putra <laurenceputra@gmail.com>
2015-04-01 14:29:38 +0000

    fixed logo, and added fb like + signup page

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,11 @@
 
 module.exports = function (router) {
     router.get('/', function (req, res) {
-        res.render('index');
+        res.render('pages/index');
+    });
+    
+    router.get('/signup', function (req, res) {
+        res.render('pages/signup');
     });
 
 };
